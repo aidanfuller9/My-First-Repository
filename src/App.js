85693b840c97c7ec1f7eaeaa2cfc38f193a05b61
aidanfuller9 <60492278+aidanfuller9@users.noreply.github.com>
2021-11-17 16:39:01 -0800

    Include year and high school

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Howdy doo</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>Year: 2021 School: Oakland Technical High School</h2>
     </div>
   );
 }
