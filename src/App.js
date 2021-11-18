458f501f2e94209bb123dca9db796394ac9d4d05
aidanfuller9 <60492278+aidanfuller9@users.noreply.github.com>
2021-11-17 17:45:54 -0800

    images

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Howdy doo</h1>
+      <h1>Howdy</h1>
       <h2>Year: 2021 School: Oakland Technical High School</h2>
+      <h3></h3>
+      <div>
+        <img
+          class="img1"
+          src="https://i.pinimg.com/originals/e6/43/64/e643645627d0b2f33c2456e2fee0398d.jpg"
+        ></img>
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const rootElement = document.getElementById("root");
 ReactDOM.render(
   <StrictMode>
     <App />
+    <App />
   </StrictMode>,
   rootElement
 );
